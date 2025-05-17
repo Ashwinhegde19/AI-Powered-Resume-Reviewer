e31115f32dadcd3d6b3a16442690d340f76f0afb
Ashwinhegde19 <ashwinhegde19@gmail.com>
2025-05-17 14:50:04 +0000

    Implement file upload and retrieval endpoints; restructure database client and collections

--- app/server.py
+++ app/server.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, UploadFile
-from uuid import uuid4
+from fastapi import FastAPI, UploadFile, Path
 from .utils.file import save_to_disk
-
+from .db.collections.files import files_collection, FileSchema
+from bson import ObjectId
 
 app = FastAPI()
 
@@ -11,12 +11,44 @@ def hello():
     return {"status": "healthy"}
 
 
+@app.get("/{id}")
+async def get_file_by_id(id: str = Path(..., description="ID of the file")):
+    db_file = await files_collection.find_one({"_id": ObjectId(id)})
+
+    print(db_file)
+
+    return {
+        "_id": str(db_file["_id"]),
+        "name": db_file["name"],
+        "status": db_file["status"],
+        "result": db_file["result"] if "result" in db_file else None,
+    }
+
+
 @app.post("/upload")
 async def upload_file(
     file: UploadFile
-
 ):
-    id = uuid4()
-    file_path = f"/mnt/uploads/{id}/{file.filename}"
+
+    db_file = await files_collection.insert_one(
+        document=FileSchema(
+            name=file.filename,
+            status="saving"
+        )
+    )
+
+    file_path = f"/mnt/uploads/{str(db_file.inserted_id)}/{file.filename}"
+
     await save_to_disk(file=await file.read(), path=file_path)
-    return {"file_id": id}
+
+    # Push to Queue
+    q.enqueue(process_file, str(db_file.inserted_id), file_path)
+
+    # MongoDB Save
+    await files_collection.update_one({"_id": db_file.inserted_id}, {
+        "$set": {
+            "status": "queued"
+        }
+    })
+
+    return {"file_id": str(db_file.inserted_id)}
